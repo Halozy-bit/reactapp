8c3b97213762a346bf575d518e8b171f7ddd7f24
Halozy-bit <jamalludin1999z@gmail.com>
2021-02-23 11:50:21 +0700

    menambahkan 3 component
    Membuat component about, contact dan home yang akan digunakan pada main.js merupakan isi tampilan dari single page nya

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Home</h2>
+                <p> ini home </p>
+            </div>
+        )
+    }
+}
